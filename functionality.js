a4415484b40411d7bfbb49a1a6a6ce0ae2b7bb40
smile995 <ah.smile995@gmai.com>
2024-02-18 22:46:05 +0600

    fixed some problem

--- functionality.js
+++ functionality.js
@@ -1,7 +1,6 @@
 // Redirect functionality when someone click on the button
 
 document.getElementById("redirectButton").addEventListener("click", function() {
-  // Smooth scrolling to the content section
   document.getElementById('content').scrollIntoView({ 
     behavior: 'smooth' 
   })
